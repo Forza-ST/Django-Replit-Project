ff4d1f1a30588658cc82a1485aeb02abefa9ecda
Zeshan <farhanayassminn@gmail.com>
2025-02-01 23:32:16 +0500

    super user login feature enabled

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from myrestaurant import views
-
-urlpatterns = [
+from django.contrib import admin
+urlpatterns =[ 
+    path('admin/', admin.site.urls),  # Default admin URL path
     path('', views.index, name='index'),
     path('new_login/', views.new_login, name='new_login'),
     path('add_dish/', views.add_dish, name='add_dish'),
--- myrestaurant/urls.py
+++ myrestaurant/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new_login/', views.new_login, name='new_login'),
     path('add_dish/', views.add_dish, name='add_dish'),
+]
